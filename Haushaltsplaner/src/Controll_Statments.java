5fbc3ddfb372289cf79509069d5757106ad033be
dominik <dominik@dfch-ludwig.de>
2015-08-08 17:26:22 +0200

    anpassen der sql statments an view in mysql

--- Haushaltsplaner/src/Controll_Statments.java
+++ Haushaltsplaner/src/Controll_Statments.java
@@ -1,8 +1,9 @@
+import java.util.Vector;
+
 public enum Controll_Statments {
 	all {
 		public String toString() {
-			// user einfügen
-			return "select * from Einkauf ein, Produkt p, Konto k, Markt m where ein.m_ID = m.M_ID AND ein.p_ID = p.P_ID AND ein.k_ID = k.K_ID AND ein.u_ID =";
+			return "select * from ViewAll;";
 		}
 	},
 
@@ -52,51 +53,46 @@ public enum Controll_Statments {
 	},
 	PreisSortierung {
 		public String toString() {
-			return "select p.Preis, p.name from Produkt p order by p.Preis;";
+			return "select * from SortPreis;";
 		}
 	},
 
 	AusgabenSortierung {
 		public String toString() {
-			return "select p.name,p.preis,ein.anzahl,ROUND((p.preis * me.anzahl),2) as gesamtpreis from Produkt p, Einkauf ein where p.P_ID = ein.p_ID;";
+			return "select * from SortAusgaben;";
 		}
 	},
 
 	DatumSortierung {
 		public String toString() {
-			return "select * from Produkt p, Einkauf ein where p.P_ID = ein.p_ID Order by ein.Datum;";
+			return "select * from SortDatum;";
 		}
 	},
 	EntfernungsSortierung {
 		public String toString() {
-			return "select m.entfernung,m.name from Markt m;";
+			return "select * from SortEntfernung;";
 		}
 	},
 
-	KontoTabelleHinzufügen {
-		public String toString() {
-			return "create table Konto(betrag float, name VARCHAR(255), bankleitzahl VARCHAR(8), kontonummer VARCHAR(9), minimum INT, K_ID INT NOT NULL AUTO_INCREMENT,PRIMARY KEY(K_ID));";
-		}
-	},
-
-	ProduktTabelleHinzufügen {
-		public String toString() {
-			return "create table Produkt(preis float, name VARCHAR(255), gewicht INT, P_ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(P_ID));";
-		}
-	},
-	MarktTabelleHinzufügen {
-		public String toString() {
-			return "create table Markt(name VARCHAR(255), postleitzahl VARCHAR(5), adresse VARCHAR(255), entfernung INT, M_ID INT NOT NULL AUTO_INCREMENT,PRIMARY KEY(M_ID));";
-		}
-	},
-
-	EinkaufTabelleHinzufügen {
-		public String toString() {
-			return "create table Einkauf(anzahl INT, datum DATE, m_ID INT, FOREIGN KEY(m_ID) REFERENCES Markt(M_ID) ON DELETE CASCADE,"
-					+ "k_ID INT, FOREIGN KEY(k_ID) REFERENCES Konto(K_ID) ON DELETE CASCADE,"
-					+ "p_ID INT,FOREIGN KEY(p_ID) REFERENCES Produkt(P_ID) ON DELETE CASCADE,"
-					+ "u_ID INT, FOREIGN KEY(u_ID) REFERENCES User(U_ID) ON DELETE CASCADE,"
-					+ "PRIMARY KEY(m_ID,p_ID,k_ID));";
+	NeuHinzufügen
+	{
+		@SuppressWarnings("null")
+		public Vector<String> toString(String kuerzel)
+		{
+			Vector<String> mstrAttNew = null;
+			mstrAttNew.add("create database HausHaltsPlaner_"+kuerzel+";");
+			mstrAttNew.add("use HausHaltsPlaner_"+kuerzel+";");
+			mstrAttNew.add("create table Einkauf like Master.Einkauf;");
+			mstrAttNew.add("create table Konto like Master.Konto;");
+			mstrAttNew.add("create table Markt like Master.Markt;");
+			mstrAttNew.add("create table Produkt like Master.Produkt;");
+			mstrAttNew.add("create view ViewAll as select * from Master.ViewAll;");
+			mstrAttNew.add("create view SortAusgaben as select * from Master.SortAusgaben;");
+			mstrAttNew.add("create view SortDatum as select * from Master.SortDatum;");
+			mstrAttNew.add("create view SortEntfernung as select * from master.SortEntfernung;");
+			mstrAttNew.add("create view SortPreis as select * from Master.SortPreis;");
+			
+			return mstrAttNew;
 		}
 	},
 	commit {
@@ -105,16 +101,5 @@ public enum Controll_Statments {
 		}
 	},
 
-	DatenbasisHinzufügen {
-		public String toString() {
-			return "create databases ";
-		}
-	},
-
-	DatenbasisBenutzen {
-		public String toString() {
-			return "use ";
-		}
-	}
 
 }
