18cf0d5d5239d916cc450f7d901cdca716c9b981
dominik <dominik@dominik-Aspire-5749>
2015-09-26 19:42:25 +0200

    add new jsp

---
+++ jsp/WebContent/View_Konto.jsp
@@ -0,0 +1,43 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<html>
+<header>
+
+     
+</header>
+
+
+<center><h2 id="KleinererText">Alles um deine Konten</h2>
+
+
+<table bgcolor="#E0F2F7" border="1" cellspacing="1" cellpadding="1">
+<tr> 
+<th> K_ID</th> 
+<th> Kontoname</th> 
+<th> Bankleitzahl </th> 
+<th> Kontonummer</th> 
+<th> Betrag</th> 
+<th> Minimum</th> 
+</tr> 
+</table>
+<p></p>
+
+
+<fieldset><legend><b>Konto Aendern/Eintragen</b></legend>
+
+<p><label>Name: <input type="text" name="i_kontoname"></label><br>
+<p><label>Kontonummer: <input type="text" name="i_kontonummer"></label><br>
+<p><label>Bankleitzahl:<input type="text" name="i_kontobankleitzahl"></label><br>
+<p><label>Minimum:<input type="text" name="i_kontominimum"></label><br>
+<p><label>Betrag:<input type="text" name="i_kontobetrag"></label><br>
+
+<input type="submit" name="i_produkteintragen" value="Eintragen">
+<input type="submit" name="i_produktaendern" value="Aendern">
+
+</fieldset>
+
+
+
+</html>
+            
---
+++ jsp/src/jsp/Model_Markt.java
@@ -0,0 +1,103 @@
+package jsp;
+
+public class Model_Markt {
+
+	// Markt Attribute
+	private String mstrName;
+	private String mstrPLZ;
+	private String mstrAdr;
+	private int mintEntfernung;
+	private int mintID;
+	private boolean mboolequal;
+
+	public boolean isMboolequal() {
+		return mboolequal;
+	}
+
+	public void setMintID(int mintID) {
+		this.mintID = mintID;
+	}
+
+	public int getMintID() {
+		return mintID;
+	}
+
+	public Model_Markt(String name, String plz, String adr, int entfernung, int id) {
+		this.mstrName = name;
+		this.mstrPLZ = plz;
+		this.mstrAdr = adr;
+		this.mintEntfernung = entfernung;
+		this.mintID = id;
+		this.mboolequal = true;
+
+	}
+
+	public Model_Markt() {
+		this.mstrName = "";
+		this.mstrPLZ = "";
+		this.mstrAdr = "";
+		this.mintEntfernung = 0;
+		this.mintID = -1;
+	}
+
+	public String getMstrName() {
+		return mstrName;
+	}
+
+	public void setMstrName(String mstrName) {
+		this.mstrName = mstrName;
+	}
+
+	public String getMstrPLZ() {
+		return mstrPLZ;
+	}
+
+	public void setMstrPLZ(String mstrPLZ) {
+		this.mstrPLZ = mstrPLZ;
+	}
+
+	public String getMstrAdr() {
+		return mstrAdr;
+	}
+
+	public void setMstrAdr(String mstrAdr) {
+		this.mstrAdr = mstrAdr;
+	}
+
+	public int getMintEntfernung() {
+		return mintEntfernung;
+	}
+
+	public void setMintEntfernung(int mintEntfernung) {
+		this.mintEntfernung = mintEntfernung;
+	}
+
+	public String SQLerstellen() {
+	
+		return Controll_Statments.AddMarkt(this.mstrName, this.mstrPLZ, this.mstrAdr, this.mintEntfernung);
+	}
+	
+	public String SQLentfernen() {
+
+		return Controll_Statments.UpdateMarkt(this.mstrName, this.mstrPLZ, this.mstrAdr, this.mintEntfernung, this.mintID);
+	}
+	public Object[] print() {
+		Object[] tmp = {this.mstrName, this.mstrPLZ , this.mstrAdr ,this.mintEntfernung,this.mintID};
+		return tmp;
+	}
+
+	public boolean equal(Model_Markt tmp) {
+		if (this.mstrName == tmp.mstrName && this.mstrPLZ == tmp.mstrPLZ && this.mstrAdr == tmp.mstrAdr
+				&& this.mintEntfernung == tmp.mintEntfernung) {
+			mboolequal = true;
+			return true;
+		}
+		mboolequal = false;
+		this.mstrName = tmp.mstrName;
+		this.mstrPLZ = tmp.mstrPLZ;
+		this.mstrAdr = tmp.mstrAdr;
+		this.mintEntfernung = tmp.mintEntfernung;
+		return false;
+	}
+
+}
